0b03c018d19b90bc5dbb1833ea4be3ccad87a99e
Jamaica <jamaica@blackeverywhere.com>
2023-02-09 12:51:01 -0500

    Add marker to databases function

--- src/components/UserLocationShareScreen.js
+++ src/components/UserLocationShareScreen.js
@@ -1,10 +1,27 @@
 import React from "react";
 import "./UserLocationShareScreen.css";
+import { db } from "../firebase";
+import { doc, addDoc, setDoc, collection } from "firebase/firestore";
+import code from "./CommunitySelector";
 
 const UserLocationShareScreen = () => {
+    const getCurrentLocation = () => {
+        console.log("called getCurrentLocation");
+        navigator.geolocation.getCurrentPosition(res => {
+            const [lat, lng] = [res.coords.latitude, res.coords.longitude];
+            addMarkerToDb(lat, lng);
+        });
+    };
+
+    const addMarkerToDb = (lat, lng) => {
+        addDoc(collection(db, "communities", "5555"), { lat, lng });
+        console.log('ran addmarker')
+    };
+
     return (
         <div>
             <h1>Share Your Location</h1>
+            <button onClick={() => getCurrentLocation()}>Share Button</button>
         </div>
     );
 };
