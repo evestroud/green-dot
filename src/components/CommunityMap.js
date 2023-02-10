c8393fec7511ed03daee09e29b4ad3809cde9c12
Eve Stroud <evestroud89@gmail.com>
2023-02-10 12:57:52 -0500

    Access and draw markers in CommunityMap

--- src/components/CommunityMap.js
+++ src/components/CommunityMap.js
@@ -1,11 +1,13 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import "./CommunityMap.css";
 import share from "./assets/share.png";
+import { collection, doc, onSnapshot, query } from "firebase/firestore";
+import { db, auth } from "../firebase"
 
 const MAPS_API_KEY = `${process.env.REACT_APP_MAPS_API_KEY}`;
 
-const CommunityMap = () => {
+const CommunityMap = ({ community }) => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: MAPS_API_KEY,
   });
@@ -13,6 +15,19 @@ const CommunityMap = () => {
   const [center, setCenter] = useState({ lat: 39, lng: -95 });
   const [zoom, setZoom] = useState(4);
   const ref = useRef(null);
+
+  useEffect(() => {
+    const q = query(collection(doc(db, "communities", community), "markers"));
+
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let markers = [];
+      QuerySnapshot.forEach((marker) => {
+        markers.push({ ...marker.data(), id: marker.id });
+      });
+      setMarkers(markers);
+      // setMyDot(markers.find((m) => m.uid === auth.currentUser.uid));
+    });
+  });
 
   return (
     <div id="community-map">
@@ -26,7 +41,14 @@ const CommunityMap = () => {
           center={center}
           zoom={zoom}
           onLoad={(map) => (ref.current = map)}
-        ></GoogleMap>
+        >
+          {markers.map(({ lat, lng, id }) => (
+            <Marker
+              position={{ lat, lng }}
+              key={id}
+            />
+          ))}
+        </GoogleMap>
       ) : (
         <h2>Loading map...</h2>
       )}
